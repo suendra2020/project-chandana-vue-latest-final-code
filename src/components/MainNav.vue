c72ab68ade5bead66d8c8e9ee268a20dba57281b
suendra2020 <68986498+suendra2020@users.noreply.github.com>
2025-07-25 07:12:47 +0000

    url updated

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,13 +3,13 @@
     <div class="container">
       <ul class="main-menu">
         <li><router-link to="/" active-class="active">Home</router-link></li>
-        <li><router-link to="/about" active-class="active">About Us</router-link></li>
-        <li><router-link to="/vision" active-class="active">Vision</router-link></li>
-        <li><router-link to="/teams" active-class="active">Teams</router-link></li>
-        <li><router-link to="/activities" active-class="active">Activities</router-link></li>
-        <li><router-link to="/reports" active-class="active">Reports</router-link></li>
-        <li><router-link to="/events" active-class="active">Events</router-link></li>
-        <li><router-link to="/contact" active-class="active">Contact Us</router-link></li>
+        <li><router-link to="https://projectchandana.com/about" active-class="active">About Us</router-link></li>
+        <li><router-link to="https://projectchandana.com/vision" active-class="active">Vision</router-link></li>
+        <li><router-link to="https://projectchandana.com/teams" active-class="active">Teams</router-link></li>
+        <li><router-link to="https://projectchandana.com/activities" active-class="active">Activities</router-link></li>
+        <li><router-link to="https://projectchandana.com/reports" active-class="active">Reports</router-link></li>
+        <li><router-link to="https://projectchandana.com/events" active-class="active">Events</router-link></li>
+        <li><router-link to="https://projectchandana.com/contact" active-class="active">Contact Us</router-link></li>
       </ul>
     </div>
   </nav>
